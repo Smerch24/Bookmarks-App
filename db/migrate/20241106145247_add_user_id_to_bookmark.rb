1d01e91d6306cae27499da6d16610ef5c89a4d3d
Ilia <teterailiya@gmail.com>
2024-11-06 17:11:07 +0200

    Add user_id reference to Bookmark

---
+++ db/migrate/20241106145247_add_user_id_to_bookmark.rb
@@ -0,0 +1,5 @@
+class AddUserIdToBookmark < ActiveRecord::Migration[7.1]
+  def change
+    add_reference :bookmarks, :user, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_04_125524) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_06_145247) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -46,6 +46,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_04_125524) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "screenshot"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_bookmarks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -65,4 +67,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_04_125524) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "bookmarks", "users"
 end
