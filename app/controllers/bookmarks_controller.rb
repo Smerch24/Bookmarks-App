24886565cb75ff0653eddae2fa2366e95f39821d
Ilia <teterailiya@gmail.com>
2024-11-06 17:25:04 +0200

    Fill in bookmarks controller

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,5 +2,46 @@
 
 # Bookmarks managment
 class BookmarksController < ApplicationController
-  def index; end
+  before_action :find_bookmark_by_id, only: %i[edit update destroy]
+  def index
+    @bookmarks = Bookmark.all
+  end
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    if @bookmark.save
+      redirect_to bookmarks_path
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update 
+    if @bookmark.update(bookmark_params)
+      redirect_to bookmarks_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy 
+    @bookmark.destroy
+    redirect_to bookmarks_path
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:title, :url_link, :description)
+  end
+
+  def find_bookmark_by_id
+    @bookmark = Bookmark.find(params[:id])
+  end
 end
