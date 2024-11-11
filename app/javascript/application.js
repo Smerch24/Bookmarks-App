90a2db86d44cf919bbaf6ac72bdc4403e166a0bc
Ilia <teterailiya@gmail.com>
2024-11-11 11:42:52 +0200

    Add dark-mode btn

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,24 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import "@hotwired/turbo-rails";
+import "controllers";
+
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleDarkModeButton = document.getElementById("toggle-dark-mode");
+
+  if (toggleDarkModeButton) {
+    toggleDarkModeButton.addEventListener("click", function () {
+      document.body.classList.toggle("dark-mode");
+
+      if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+      } else {
+        localStorage.setItem("theme", "light");
+      }
+    });
+
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme === "dark") {
+      document.body.classList.add("dark-mode");
+    }
+  }
+});
